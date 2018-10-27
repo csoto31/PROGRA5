c05de6ec881c1fb985d46129c985b96b02e12a33
csoto31 <43452597+csoto31@users.noreply.github.com>
2018-10-27 13:29:33 -0600

    AgregadoTrabajoEnClase

---
+++ PrograAvanzada.TrabajoEnClase.Northwind/PrograAvanzadaWeb.TrabajoEnClase.Northwind/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="PrograAvanzadaWeb.TrabajoEnClase.Northwind.MvcApplication" Language="C#" %>
---
+++ PrograAvanzada.TrabajoEnClase.Northwind/PrograAvanzadaWeb.TrabajoEnClase.Northwind/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(PrograAvanzadaWeb.TrabajoEnClase.Northwind.Startup))]
+namespace PrograAvanzadaWeb.TrabajoEnClase.Northwind
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
